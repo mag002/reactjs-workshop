53f7c39f1fc160b6e999f9b874eb3b5322c82b0f
mag002 <lehongphuc7394@gmail.com>
2019-08-09 12:26:07 +0700

    add event to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,53 +1,63 @@
 import React from "react";
 import CartQuantity from "./CartQuantity";
 
-export default function Cart() {
-  return (
-    <>
-      <CartQuantity outside/>
-      <div className="cart ">
-        <div className="close-cart">X</div>
-        <div className="title">
-          <CartQuantity>s</CartQuantity>
-          <h4>Cart</h4>
-        </div>
-        <hr />
+export default class Cart extends React.Component {
+  state = {
+    open: false
+  };
+  toggleCart = () => {
+    this.setState({ open: !this.state.open });
+  };
+  render() {
+    return (
+      <>
+        <CartQuantity outside click={this.toggleCart} />
+        <div className={`cart ${this.state.open ? "active" : ""}`}>
+          <div className="close-cart" onClick={this.toggleCart}>
+            X
+          </div>
+          <div className="title">
+            <CartQuantity />
+            <h4>Cart</h4>
+          </div>
+          <hr />
 
-        <div className="cart__items">
-          <div className="cart__item d-flex">
-            <img
-              src="https://k300.vn/upload/images/2019/T7/5-AK/T2012-6.jpg"
-              alt=""
-            />
-            <div className="cart__item__info">
-              <h5 className="cart__item__title">Skull</h5>
-              <p className="cart__item__size">Size: X</p>
-              <p className="cart__item__quantity">Quantity: 2</p>
+          <div className="cart__items">
+            <div className="cart__item d-flex">
+              <img
+                src="https://k300.vn/upload/images/2019/T7/5-AK/T2012-6.jpg"
+                alt=""
+              />
+              <div className="cart__item__info">
+                <h5 className="cart__item__title">Skull</h5>
+                <p className="cart__item__size">Size: X</p>
+                <p className="cart__item__quantity">Quantity: 2</p>
+              </div>
+              <div className="cart__item__price">14$</div>
+              <div className="remove-cart-item">
+                <i className="fas fa-times" />
+              </div>
             </div>
-            <div className="cart__item__price">14$</div>
-            <div className="remove-cart-item">
-              <i className="fas fa-times" />
+          </div>
+          <div className="cart__items">
+            <div className="cart__item d-flex">
+              <img
+                src="https://k300.vn/upload/images/2019/T7/5-AK/T2012-6.jpg"
+                alt=""
+              />
+              <div className="cart__item__info">
+                <h5 className="cart__item__title">Skull</h5>
+                <p className="cart__item__size">Size: X</p>
+                <p className="cart__item__quantity">Quantity: 2</p>
+              </div>
+              <div className="cart__item__price">14$</div>
+              <div className="remove-cart-item">
+                <i className="fas fa-times" />
+              </div>
             </div>
           </div>
         </div>
-        <div className="cart__items">
-          <div className="cart__item d-flex">
-            <img
-              src="https://k300.vn/upload/images/2019/T7/5-AK/T2012-6.jpg"
-              alt=""
-            />
-            <div className="cart__item__info">
-              <h5 className="cart__item__title">Skull</h5>
-              <p className="cart__item__size">Size: X</p>
-              <p className="cart__item__quantity">Quantity: 2</p>
-            </div>
-            <div className="cart__item__price">14$</div>
-            <div className="remove-cart-item">
-              <i className="fas fa-times" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
--- src/components/CartQuantity.js
+++ src/components/CartQuantity.js
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function CartQuantity(props) {
   return (
-    <div className={`cart-quantity ${props.outside ? "outside" : ""}`}>
+    <div
+      className={`cart-quantity ${props.outside ? "outside" : ""}`}
+      onClick={props.click}
+    >
       <div className="cart-img">
         <div className="cart-number">2</div>
       </div>
